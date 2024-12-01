0394b0ca05d5b02a0091d042ba27a8ab55f237c4
Navneet_thakur <127828104+navneet451@users.noreply.github.com>
2024-12-01 13:51:36 +0530

    Create HomePage.jsx

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h2>Home Page (Private)</h2>
+      <p>Welcome to the private home page!</p>
+    </div>
+  );
+}
+
+export default HomePage;
